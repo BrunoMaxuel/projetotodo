dfdad32a0e1518329d0dc375ae2bb1a69aff19df
Bruno Maxuel <brunomaxuel2@gmail.com>
2023-07-17 06:27:58 -0300

    O erro anterior era o model com nome Tesk que deveria ser Task

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,15 +4,17 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\category;
+use App\Models\Task;
 
 class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
         $this->call([
-            UserSeeder::class,
-            CategorySeeder::class,
-            TaskSeeder::class
+            UserSeeder::class
         ]);
+        Category::factory(50)->create();
+        Task::factory(50)->create();
     }
 }
